c87675622d1a223b3440368f0683967590367bc3
jh3y <jhey.tompkins@gmail.com>
2015-01-16 18:43:25 +0000

    Initial commit

---
+++ dist/zoomable.js
@@ -0,0 +1,56 @@
+/*
+zoomable - http://jh3y.github.io/zoomable
+Licensed under the MIT license
+
+jh3y (c) 2014.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+(function() {
+  var zoomable;
+
+  window.zoomable = zoomable = function(element) {
+    var endZoom, newZoom, startZoom, zoom;
+    if (!(this instanceof zoomable)) {
+      return new zoomable(element);
+    }
+    this.element = element;
+    newZoom = this;
+    newZoom.scale = parseFloat(newZoom.element.getAttribute('zoom-scale'));
+    newZoom._scale = 1.00;
+    zoom = function(e) {
+      var diff, oldDist;
+      if (e.touches && e.touches.length === 2) {
+        e.preventDefault();
+        oldDist = newZoom.dist;
+        newZoom.dist = Math.sqrt((e.touches[0].clientX - e.touches[1].clientX) * (e.touches[0].clientX - e.touches[1].clientX) + (e.touches[0].clientY - e.touches[1].clientY) * (e.touches[0].clientY - e.touches[1].clientY));
+        diff = newZoom.dist - oldDist;
+        if (newZoom.dist < oldDist) {
+          newZoom._scale = newZoom._scale - newZoom.scale;
+        } else if (newZoom.dist > oldDist) {
+          newZoom._scale = newZoom._scale + newZoom.scale;
+        }
+        return newZoom.element.setAttribute('style', '-webkit-transform: scale(' + newZoom._scale + '); transform: scale(' + newZoom._scale + ');');
+      }
+    };
+    startZoom = function(e) {
+      return newZoom.element.addEventListener("touchmove", zoom, true);
+    };
+    endZoom = function() {
+      return newZoom.element.removeEventListener("touchmove", zoom, true);
+    };
+    newZoom.element.addEventListener("touchstart", startZoom, false);
+    newZoom.element.addEventListener("touchend", endZoom, false);
+    return this;
+  };
+
+  [].forEach.call(document.querySelectorAll('[zoomable]'), function(zoomer) {
+    return new zoomable(zoomer);
+  });
+
+}).call(this);
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp'),
+	coffee = require('gulp-coffee'),
+	jade = require('gulp-jade'),
+	connect = require('gulp-connect'),
+	plumber = require('gulp-plumber'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	deploy = require('gulp-gh-pages'),
+	filter = require('gulp-filter'),
+	sources = {
+		coffee: "src/coffee/**/*.coffee",
+		jade: 'src/jade/**/*.jade',
+		docs: "src/jade/*.jade",
+		overwatch: "./out/**/*.*",
+		license: 'src/license/*.txt',
+		build: 'out/js/*.js'
+	},
+	destinations = {
+		js: './out/js/',
+		docs: './out/',
+		dist: './dist/'
+	};
+gulp.task('reload', function(event) {
+	return gulp.src(sources.overwatch)
+		.pipe(connect.reload());
+});
+gulp.task('serve', ['build'], function(event) {
+	connect.server({
+		root: destinations.docs,
+		port: 1987,
+		livereload: true
+	});
+	gulp.watch(sources.overwatch, ['reload']);
+});
+gulp.task('coffee:compile', function(event) {
+	var coffeeFilter = filter('**/*.coffee');
+	return gulp.src([sources.license, sources.coffee])
+		.pipe(plumber())
+		.pipe(coffeeFilter)
+		.pipe(concat('zoomable.coffee'))
+		.pipe(coffee())
+		.pipe(coffeeFilter.restore())
+		.pipe(concat('zoomable.js'))
+		.pipe(gulp.dest(destinations.js))
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(destinations.js));
+});
+gulp.task('coffee:watch', function(event) {
+	gulp.watch(sources.coffee, ['coffee:compile']);
+});
+gulp.task('jade:compile', function(event) {
+	return gulp.src(sources.docs)
+		.pipe(plumber())
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest(destinations.docs));
+});
+gulp.task('jade:watch', function(event){
+	gulp.watch(sources.jade, ['jade:compile']);
+});
+gulp.task('dist', ['coffee:compile'], function(event) {
+	return gulp.src(sources.build)
+		.pipe(gulp.dest(destinations.dist));
+});
+gulp.task('build', ['jade:compile', 'coffee:compile']);
+
+gulp.task('deploy', ['build'], function () {
+	return gulp.src("out/**/*.*")
+	.pipe(deploy());
+});
+
+gulp.task('default', ["serve", "jade:watch", "coffee:watch"]);
